a26e6f25d750f56d0cdca11cba7cf01c765e8f24
Alencar Avelar <alencarhlavelar@gmail.com>
2025-09-20 15:32:31 -0300

    Criação da Classe C e Criado o Método MA3()

---
+++ C.java
@@ -0,0 +1,27 @@
+public class B {
+    private String c1;
+    private int c2;
+
+    public int getC1() {
+        return c1;
+    }
+
+    public getC2(){
+        return c2;
+    }
+
+    public void setC1(Sring c1){
+        this.c1 = c1;
+    }
+
+    public void setC2(float c2){
+        this.c2 = c2;
+    }
+
+    public void MC1 (){
+        System.out.println("Metodo C2 executado");
+    }
+    public void MC2(){
+        System.out.println("Metodo C1 executado");
+    }
+}
