c37c35f4c4afcacf073d38045827b4b3ea485939
Alencar Avelar <alencarhlavelar@gmail.com>
2025-09-19 22:38:00 -0300

    arquivos A e B com atributos getters e setters

---
+++ A.java
@@ -0,0 +1,30 @@
+
+public class A {
+    private int a1;
+    private float a2;
+
+
+    public int getA1() {
+        return a1;
+    }
+    public void setA1(int a1) {
+        this.a1 = a1;
+    }
+
+    public float getA2() {
+        return a2;
+    }
+
+    public void setA2(float a2) {
+        this.a2 = a2;
+    }
+
+    public void MA1(){
+        System.out.println("Metodo MA1 executado");
+    }
+
+    public void MA2(){
+        System.out.println("Metodo MA2 executado");
+    }
+
+}
---
+++ B.java
@@ -0,0 +1,27 @@
+public class B {
+    private int b1;
+    private float b2;
+
+    public int getB1() {
+        return b1;
+    }
+
+    public getB2(){
+        return b2;
+    }
+
+    public void setB1(int b1){
+        this.b1 = b1;
+    }
+
+    public void setB2(float b2){
+        this.b2 = b2;
+    }
+
+    public void MB1 (){
+        System.out.println("Metodo B2 executado");
+    }
+    public void MB2(){
+        System.out.println("Metodo B1 executado");
+    }
+}
